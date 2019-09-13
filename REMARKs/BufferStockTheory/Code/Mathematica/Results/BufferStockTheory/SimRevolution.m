a53a245eefab0be83b61dafd72b5d79b73eb9586
llorracc <llorracc@users.noreply.github.com>
2019-09-12 22:24:15 -0400

    BufferStockTheory Folder delete

--- REMARKs/BufferStockTheory/Code/Mathematica/Results/BufferStockTheory/SimRevolution.m
+++
@@ -1,8 +0,0 @@
-(* ::Package:: *)
-
-Print["Running SimRevolution.m"];
-pRattList[[-1]] = Table[1,{Length[pRattList[[-1]]]}];
-pLevtList[[-1]] = Table[pLevtMean[[-1]], {Length[pLevtList[[-1]]]}];
-
-KeepSimulating[21];
-
--- REMARKs/BufferStockTheory/do_all.py
+++
@@ -1,7 +0,0 @@
-# do_all.py is required for a REMARK; in this case, it just runs BufferStockTheory.py
-
-import BufferStockTheory.py
-
-
-
-
