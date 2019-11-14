ca77eb36232466cda45a2be017879e3cbea194c4
sb <sbenthall@gmail.com>
2019-11-14 14:53:07 -0500

    do_all.py now

---
+++ REMARKs/BlanchardPA2019/do_all.py
@@ -0,0 +1,9 @@
+import os
+
+os.system("jupytext --to py Code/Python/BlanchardPA2019.ipynb")
+
+import importlib.util
+spec = importlib.util.spec_from_file_location("Blanchard2019", "Code/Python/Blanchard.py")
+Blanchard2019 = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(Blanchard2019)
+
--- REMARKs/BlanchardPA2019/do_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-jupytext --to py BlanchardPA2019.ipynb
-python BlanchardPA2019.py
-
