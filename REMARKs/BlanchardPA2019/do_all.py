12c68ecc1118e6ca07d17f1adadc5a8e25caba04
sb <sbenthall@gmail.com>
2019-11-14 14:58:06 -0500

    correct python path in Blanchard do_all

--- REMARKs/BlanchardPA2019/do_all.py
+++ REMARKs/BlanchardPA2019/do_all.py
@@ -3,7 +3,7 @@ import os
 os.system("jupytext --to py Code/Python/BlanchardPA2019.ipynb")
 
 import importlib.util
-spec = importlib.util.spec_from_file_location("Blanchard2019", "Code/Python/Blanchard.py")
+spec = importlib.util.spec_from_file_location("Blanchard2019", "Code/Python/BlanchardPA2019.py")
 Blanchard2019 = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(Blanchard2019)
 
