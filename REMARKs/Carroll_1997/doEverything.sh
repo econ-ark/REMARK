9c30d020a0d1fe9b0af3b3258808e0b0488a1f99
sonjeongwon621 <sonjeongwon621@gmail.com>
2020-01-07 23:31:12 -0500

    Changed xelatex to pdflatex. Changed doEverything.sh so that slides are created with pdflatex and not makePDF-Shareable.sh

--- REMARKs/Carroll_1997/doEverything.sh
+++ REMARKs/Carroll_1997/doEverything.sh
@@ -7,13 +7,10 @@ sudo echo 'Authorizing sudo.'
 python ./Carroll_1997_RemARK.py #save figures and tables
 
 cd Paper
-xelatex main.tex #creates the main paper and aux file
+pdflatex main.tex #creates the main paper and aux file
 bibtex main.aux  #run bibtex to run aux to create bbl file
-xelatex main.tex #rerun latex with the bbl file
+pdflatex main.tex #rerun latex with the bbl file
 
 cd ..
 cd Slides
-
-chmod u+x make_slides.sh #This is to grant permission to making the slides
-
-./make_slides.sh #create slides
+pdflatex slides.tex
