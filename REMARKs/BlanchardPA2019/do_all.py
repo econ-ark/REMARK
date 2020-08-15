81a886897d751dd0c4ea6ee8488c7745d6543888
Mridul Seth <seth.mridul@gmail.com>
2020-08-16 00:38:55 +0530

    moved BlanchardPA2019 to econ-ark/BlanchardPA2019

--- REMARKs/BlanchardPA2019/do_all.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.system("jupytext --to py Code/Python/BlanchardPA2019.ipynb")
-
-import importlib.util
-spec = importlib.util.spec_from_file_location("Blanchard2019", "Code/Python/BlanchardPA2019.py")
-Blanchard2019 = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(Blanchard2019)
-
