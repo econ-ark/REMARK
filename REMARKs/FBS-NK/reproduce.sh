e550fdf3628da2e19f9c228982c5fd19ba5946aa
wdu9 <71188956+wdu9@users.noreply.github.com>
2021-05-14 00:19:13 -0400

    Add files via upload

--- REMARKs/FBS-NK/reproduce.sh
+++ REMARKs/FBS-NK/reproduce.sh
@@ -4,5 +4,5 @@ python3 -m pip install --quiet -r requirements.txt
 
 # navigate to code/ and execute the python file to create figures
 cd ./code/python
-jupyter nbconvert --to python FBS-NK-model.ipynb
+jupyter notebook FBS-NK-model.ipynb
 ipython Jacobian-SS.py
